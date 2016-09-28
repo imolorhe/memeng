0b232903e6d8978b34d99fa74c5aa38057029bb7
Samuel Imolorhe <samuelimolo4real@gmail.com>
2016-09-28 16:06:38 +0100

    Added gulp tasks and npm packages for compiling SCSS.

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,29 @@
+let path = require('path');
+
 let gulp = require('gulp');
 let sass = require('gulp-sass');
 let uglify = require('gulp-uglify');
+let notify = require('gulp-notify');
 
+function notifyMe(msg) {
+	let opts = {
+		title: 'MemeNG',
+		subtitle: 'Meme generator',
+		message: msg.message || msg,
+		sound: 'Frog',
+		icon: path.join(__dirname, 'public/img/memeng_logo.png')
+	};
+
+	return notify(opts);
+};
 
 gulp.task('sass', () => {
-    return gulp.src('scss/styles.scss')
-        .pipe(sass())
-        .pipe(gulp.dest('public/css'))
-        .pipe(notify({message: 'SCSS compiled!'}));
+	return gulp.src('scss/styles.scss')
+		.pipe(sass()).on('error', sass.logError)
+		.pipe(gulp.dest('public/css'))
+		.pipe(notifyMe({message: 'SCSS compiled!'}));
+});
+
+gulp.task('watch', () => {
+	gulp.watch('scss/**/*.scss', ['sass']);
 });

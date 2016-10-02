5dd8665c7f636b283d72492609f91afa1c73c781
Samuel Imolorhe <samuelimolo4real@gmail.com>
2016-10-02 03:17:52 +0100

    Made key file path relative, to make it easier to configure.

--- index.js
+++ index.js
@@ -8,6 +8,7 @@
 require('dotenv').config({silent: true});
 
 process.env['PROJECT_ROOT'] = __dirname;
+process.env['FIREBASE_SERVICE_ACCOUNT_JSON_PATH'] = __dirname + process.env.FIREBASE_SERVICE_ACCOUNT_JSON_RELATIVE_PATH;
 
 // Start the app
 require('./app/app');

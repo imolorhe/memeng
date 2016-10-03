36cdfca1f4d69cb008149bc6341d5b02dd1eaf88
Samuel Imolorhe <samuelimolo4real@gmail.com>
2016-10-03 05:03:00 +0100

    Added immediate response to slack bot. Used response_url method to send information to slack. Added in channel response type.

--- app/routes/bot.js
+++ app/routes/bot.js
@@ -4,6 +4,7 @@
 
 'use strict';
 
+let request = require('request');
 let express = require('express');
 let router = express.Router();
 
@@ -30,24 +31,39 @@ router.post('/slack', (req, res) => {
 	if(req.body.text){
 		let response = req.body.text;
 		let responseSet = response.split(/\s+/);
+		let responseUrl = req.body.response_url;
 
 		// Split the text into the appropriate parameters
 		if(responseSet[0] && responseSet[1]){
 			let memeID = responseSet[0];
 			let texts = responseSet[1].split('/');
 
+			// Respond to the user while still carrying out the task
+			res.send('Got it!');
+
 			memeng.createMeme({id: memeID, top: texts[0], bottom: texts[1]}).then((memeData) => {
 
 				var result = {
 					attachments: [
 						{
+							response_type: "in_channel",
 							fallback: texts[0] + ' ' + texts[1] + ' from MemeNG.',
 							color: '#123456',
 							image_url: memeData.url
 						}
 					]
 				};
-				res.send(result);
+				request({
+					uri: responseUrl,
+					method: 'POST',
+					form: result
+				}, (err, response, body) => {
+					console.log('Request sent. ', response);
+					if(err){
+						console.log('Oops. Error: ', err.message);
+					}
+				});
+				// res.send(result || 'Got it!');
 				return false;
 			}).catch((err) => {
 				res.send('Ouch. An error. ' + err.message);
